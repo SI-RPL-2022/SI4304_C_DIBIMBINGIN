98963d1d1d3a460d3a0170828622a84b063f093c
yuardian <yunusardi28@gmail.com>
2022-04-13 00:37:36 +0700

    Bimbil

--- Dibimbingin/resources/views/bimbel.blade.php
+++ Dibimbingin/resources/views/bimbel.blade.php
@@ -1,5 +1,75 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Bimbel</h1>
+<p class="text-center fs-2 fw-bolder ">Pilih Tingkat Studimu</p>
+<p class="text-center">Cari tahu info dan program bimbingan sesuai dengan tingkat studi.</p>
+
+<div class="card-group">    
+<div class="card" style="width: 29rem">
+  <img src="smp.png" class="card-img" alt="...">
+  <a class="navbar-brand text-white" href="#">
+  <div class="card-img-overlay">
+    <h5 class="card-title" >SMP</h5>
+  </div>
+</div>
+
+<div class="card" style="width: 29rem">
+  <img src="smp.png" class="card-img" alt="...">
+  <a class="navbar-brand text-white" href="#">
+  <div class="card-img-overlay">
+    <h5 class="card-title">SMA</h5>
+  </div>
+ </div>
+</div>
+
+<div class="Title">
+<h1>Info Beasiswa Kuliah</h1>
+<p1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </br>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an.</p1>
+</div>
+<br>
+
+<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+    <!-- <div class="card">
+    <img style="height: 175px;" src="nf.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Nurul Fikri</h5>
+        <h6 class="card-subtitle mb-2 text-muted">SMA,SMK/Sederajat</h6>
+        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+    </div>
+    </div>   -->
+
+
+
+<br>
+<div class="text-center">
+<h3>Lihat Beasiswa Lain</h3>
+</div>
+
 @endsection
