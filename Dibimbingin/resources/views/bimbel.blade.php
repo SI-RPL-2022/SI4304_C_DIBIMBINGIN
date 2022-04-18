484ff59c48280f8c021271943dfb9dca005d9e02
yuardian <yunusardi28@gmail.com>
2022-04-18 13:28:51 +0700

    Pembaharuan Tampilan Page Bimbel ref #3

--- Dibimbingin/resources/views/bimbel.blade.php
+++ Dibimbingin/resources/views/bimbel.blade.php
@@ -10,47 +10,48 @@
   <a class="navbar-brand text-white" href="#">
   <div class="card-img-overlay">
     <h5 class="card-title" >SMP</h5>
+    </a>
   </div>
 </div>
 
 <div class="card" style="width: 29rem">
-  <img src="smp.png" class="card-img" alt="...">
+  <img src="sma.png" class="card-img" alt="...">
   <a class="navbar-brand text-white" href="#">
   <div class="card-img-overlay">
     <h5 class="card-title">SMA</h5>
+    </a>
   </div>
  </div>
 </div>
-
-<div class="Title">
-<h1>Info Beasiswa Kuliah</h1>
-<p1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </br>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an.</p1>
-</div>
 <br>
-
+<br>
+<br>
+<br>
+<br>
+  
 <div class="card-group">
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="nf.jpg" height= "280px" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <p class="card-text"><small class="text-muted">Kelas 1, 2, 3 SMP</small></p>
+      <h5 class="card-title">Nurul Fikri</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="go.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <p class="card-text"><small class="text-muted">Kelas 1, 2, 3 SMP</small></p>
+      <h5 class="card-title">Ganesha Opration</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="prima.jpeg" class="card-img-top" alt="..." >
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <p class="card-text"><small class="text-muted">Kelas 1, 2, 3 SMP</small></p>
+      <h5 class="card-title">Primagama</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
 </div>
