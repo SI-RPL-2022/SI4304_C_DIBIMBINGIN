0fbc42e044b7cfb24459ae17e468e7696bacf8e0
yuardian <yunusardi28@gmail.com>
2022-04-18 13:38:49 +0700

    tambahan lagi ref #3

--- Dibimbingin/resources/views/bimbel.blade.php
+++ Dibimbingin/resources/views/bimbel.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
+<br>
+<br>
+<br>
+
+
 <p class="text-center fs-2 fw-bolder ">Pilih Tingkat Studimu</p>
 <p class="text-center">Cari tahu info dan program bimbingan sesuai dengan tingkat studi.</p>
 
@@ -28,7 +33,10 @@
 <br>
 <br>
 <br>
-  
+
+<p class="text-center fs-2 fw-bolder ">Tempat Bimbingan untuk siswa SMP</p>
+<p class="text-center">Program bimbingan ampuh untuk siswa SMP yang akan membantu siswa.
+Temukan program yang cocok denganmu!</p>
 <div class="card-group">
   <div class="card">
     <img src="nf.jpg" height= "280px" class="card-img-top" alt="...">
@@ -56,21 +64,49 @@
   </div>
 </div>
 
-    <!-- <div class="card">
-    <img style="height: 175px;" src="nf.jpg" class="card-img-top" alt="...">
+<br>
+<div class="text-center">
+<h3>Lihat Bimbel Lain</h3>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<p class="text-center fs-2 fw-bolder ">Tempat Bimbingan untuk siswa SMA</p>
+<p class="text-center">Program bimbingan ampuh untuk siswa SMP yang akan membantu siswa.
+Temukan program yang cocok denganmu!</p>
+<div class="card-group">
+  <div class="card">
+    <img src="nf.jpg" height= "280px" class="card-img-top" alt="...">
     <div class="card-body">
-    <h5 class="card-title">Nurul Fikri</h5>
-        <h6 class="card-subtitle mb-2 text-muted">SMA,SMK/Sederajat</h6>
-        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p class="card-text"><small class="text-muted">Kelas 1, 2, 3 SMA</small></p>
+      <h5 class="card-title">Nurul Fikri</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
+  </div>
+  <div class="card">
+    <img src="go.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text"><small class="text-muted">Kelas 1, 2, 3 SMA/SMK/Sederajat</small></p>
+      <h5 class="card-title">Ganesha Opration</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
-    </div>   -->
-
-
+  </div>
+  <div class="card">
+    <img src="prima.jpeg" class="card-img-top" alt="..." >
+    <div class="card-body">
+      <p class="card-text"><small class="text-muted">Kelas 1, 2, 3 SMA/SMK/Sederajat</small></p>
+      <h5 class="card-title">Primagama</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+  </div>
+</div>
 
 <br>
 <div class="text-center">
-<h3>Lihat Beasiswa Lain</h3>
+<h3>Lihat Bimbel Lain</h3>
 </div>
 
 @endsection
