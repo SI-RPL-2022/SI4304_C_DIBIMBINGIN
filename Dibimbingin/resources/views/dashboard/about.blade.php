a83ac7039752a6214e5235bb3a3b6d5462da8b84
raihandaffa303 <raihandaffa303@gmail.com>
2022-05-29 20:37:38 +0700

    dashboard : about dan list beasiswa

---
+++ Dibimbingin/resources/views/dashboard/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+            <h1 class="h1 text-center" style="font-size: 50px;">
+                Ayo <b class="" style="font-weight: bold">Dibimbingin </b>  Aja
+            </h1>
+    <p class="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad atque culpa debitis et excepturi explicabo illo,vero voluptatem! Libero.
+    </p>
+
+            <img class="m-2 d-block mx-auto" src="{{asset('/about.png')}}" alt="">
+
+
+@endsection
