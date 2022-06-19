54b5161d4a4c883d53bd2d88400258d650d10575
Hamim Maafifa Nugraha <hamimnugraha001@gmail.com>
2022-06-20 00:12:34 +0700

    Rombak overview admin

--- Dibimbingin/resources/views/dash/overview.blade.php
+++ Dibimbingin/resources/views/dash/overview.blade.php
@@ -8,46 +8,39 @@
             <div class="card p-4 mx-5 mt-5">
                 <h3 class=" mx-5 mt-3 ">Overview</h3>
 
-                <div class="d-flex mt-5">
-                    <div class="info-box bg-info mx-3">
+                <div class="row mt-5">
+                    <div class="col-sm-5 info-box bg-info p-2">
                         <span class="info-box-icon bg-info"><i class="fas fa-users"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah User</span>
                             <span class="info-box-number">{{$user}}</span>
                         </div>
                     </div>
-
-                    <div class="info-box bg-warning mx-3">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-5 info-box bg-warning p-2">
                         <span class="info-box-icon bg-warning"><i class="fas fa-school"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Bimbel Sma</span>
                             <span class="info-box-number">{{$bimbelsma}}</span>
                         </div>
                     </div>
-
-                    <div class="info-box bg-success mx-3">
+                    <!-- <div class="col-sm-"></div> -->
+                    <div class="col-sm-5 info-box bg-success p-2">
                         <span class="info-box-icon bg-success"><i class="fas fa-graduation-cap"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Bimbel Smp</span>
                             <span class="info-box-number">{{$bimbelsmp}}</span>
                         </div>
                     </div>
-
-                    <div class="info-box bg-info mx-3">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-5 info-box bg-info p-2">
                         <span class="info-box-icon bg-info"><i class="fas fa-ticket-alt"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Beasiswa</span>
                             <span class="info-box-number">{{$beasiswa}}</span>
                         </div>
                     </div>
-
-                
-
-                </div>
-
-
-                <div class="d-flex mt-5">
-                    <div class="info-box bg-danger mx-3">
+                    <div class="col-sm-5 info-box bg-danger p-2">
                         <span class="info-box-icon bg-danger"><i class="fas fa-users"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Tryout</span>
@@ -55,6 +48,9 @@
                         </div>
                     </div>
 
+                
+
+                </div>
             </div>
         </div>
     </section>
