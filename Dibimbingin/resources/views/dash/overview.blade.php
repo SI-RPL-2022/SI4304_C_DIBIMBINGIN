27b4c79322795b69f524c06285be39316bf9581c
yuardian <yunusardi28@gmail.com>
2022-06-20 12:51:05 +0700

    Overviews

--- Dibimbingin/resources/views/dash/overview.blade.php
+++ Dibimbingin/resources/views/dash/overview.blade.php
@@ -20,38 +20,39 @@
                     <div class="col-sm-5 info-box bg-warning p-2">
                         <span class="info-box-icon bg-warning"><i class="fas fa-school"></i></span>
                         <div class="info-box-content">
-                            <span class="info-box-text">Jumlah Bimbel Sma</span>
+                            <span class="info-box-text">Jumlah Bimbel SMA</span>
                             <span class="info-box-number">{{$bimbelsma}}</span>
                         </div>
                     </div>
 
                     <!-- <div class="col-sm-"></div> -->
                     <div class="col-sm-5 info-box bg-success p-2">
-                        <span class="info-box-icon bg-success"><i class="fas fa-graduation-cap"></i></span>
+                        <span class="info-box-icon bg-success"><i class="fas fa-school"></i></span>
                         <div class="info-box-content">
-                            <span class="info-box-text">Jumlah Bimbel Smp</span>
+                            <span class="info-box-text">Jumlah Bimbel SMP</span>
                             <span class="info-box-number">{{$bimbelsmp}}</span>
                         </div>
                     </div>
 
                      <div class="col-sm-2"></div>
-                    <div class="col-sm-5 info-box bg-warning p-2">
-                        <span class="info-box-icon bg-warning"><i class="fas fa-school"></i></span>
+                    <div class="col-sm-5 info-box bg-primary p-2">
+                        <span class="info-box-icon bg-primary"><i class="fas fa-ticket-alt"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Event</span>
                             <span class="info-box-number">{{$event}}</span>
                         </div>
                     </div>
 
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-5 info-box bg-info p-2">
-                        <span class="info-box-icon bg-info"><i class="fas fa-ticket-alt"></i></span>
+                    
+                    <div class="col-sm-5 info-box bg-secondary p-2">
+                        <span class="info-box-icon bg-secondary"><i class="fas fa-graduation-cap"></i></span>
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Beasiswa</span>
                             <span class="info-box-number">{{$beasiswa}}</span>
                         </div>
                     </div>
 
+                    <div class="col-sm-2"></div>
                     <div class="col-sm-5 info-box bg-danger p-2">
                         <span class="info-box-icon bg-danger"><i class="fas fa-users"></i></span>
                         <div class="info-box-content">
